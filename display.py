d92dc29cc319231c82ce3faa1f2a84554b474297
Patrick Carroll <Patrick Carroll>
2017-05-01 22:00:17 -0700

    refactor make_graph into an abstract function before adding code to display three simultaneous graphs

--- display.py
+++ display.py
@@ -1,9 +1,8 @@
-import numpy as np
+#import numpy as np
 import matplotlib.pyplot as plt
 import re
 
 plt.rcdefaults()
-figure, ax = plt.subplots()
 
 regex = (r'\d{,2}\.?\d{,2}%')
 
@@ -16,11 +15,7 @@ def extract_data(country):
 			intdata.append(float(line[:len(line)-1]))
 		return intdata
 
-America = extract_data("America")
-China = extract_data("China")
-India = extract_data("India")
-
-# data here
+# axis label here
 groups = (
 "0-14 years",
 "15-24 years",
@@ -29,16 +24,26 @@ groups = (
 "65 and over"
 )
 
-y_pos = np.arange(len(groups))
-performance = America
+def make_graph(country, set_color):
+	figure, ax = plt.subplots()
+	y_data = extract_data(country)
+	#y_pos = np.arange(len(groups))
+	y_pos = [0, 1, 2, 3, 4]
 
-ax.bar(y_pos, performance, align='center', 
-	color='green', ecolor='blue')
-ax.set_xticks(y_pos)
-ax.set_ylim([0,100])
-ax.set_xticklabels(groups)
-ax.set_ylabel('Percentage')
-ax.set_title('Age of Population in America')
+	ax.bar(y_pos, y_data, align='center', 
+	color=set_color, ecolor='black')
+	ax.set_xticks(y_pos)
+	ax.set_ylim([0,100])
+	ax.set_xticklabels(groups)
+	ax.set_ylabel('Percentage')
+	ax.set_title('Age of Population in {}'.format(country))
+
+
+
+
+make_graph("America", "green")
+#make_graph("China", "red")
+#make_graph("India", "blue")
 
 plt.show()
 
