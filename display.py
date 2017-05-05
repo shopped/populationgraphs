eeb0d24059cbc52ee635a8c73fc8d0e87d264a2f
Patrick Carroll <Patrick Carroll>
2017-05-05 11:07:56 -0700

    reformatted display.py for easier call from another script

--- display.py
+++ display.py
@@ -2,18 +2,16 @@
 import matplotlib.pyplot as plt
 import re
 
-plt.rcdefaults()
-plt.suptitle('(CIA FACTBOOK 2016)')
-
 regex = (r'\d{,2}\.?\d{,2}%')
 
+
 def extract_data(country):
-	with open('parsed_age_data.txt') as text:
+	with open('parsed_age_data.json') as text:
 		matching = [s for s in text if country in s]
 		strdata = re.findall(regex, matching[0])
 		intdata = []
 		for line in strdata:
-			intdata.append(float(line[:len(line)-1]))
+			intdata.append(float(line[:len(line)-1])) # Creates an array with the five age percentages, in float form
 		return intdata
 
 # axis label here
@@ -32,31 +30,40 @@ def make_graph(country, set_color, i):
 	ax = plt.subplot2grid((t, t), (0, i), rowspan=t)
 	y_data = extract_data(country)
 	#y_pos = np.arange(len(groups))
-	y_pos = [0, 1, 2, 3, 4]
+	y_pos = [0,1,2,3,4]
 
 	ax.bar(y_pos, y_data, align='center', 
 	color=set_color, ecolor='black')
 	ax.set_xticks(y_pos)
 	ax.set_ylim([0,50])
 	ax.set_xticklabels(groups)
-	#ax.set_ylabel('Percentage')
 	ax.set_title('Age of Population in {}'.format(country))
 
-	for i, v in enumerate(y_data):
-		ax.text(i - .4, v + .3, str(v) + '%', color=set_color, fontweight='bold')
+	for i, v in enumerate(y_data): #gets x and y for the value of the bar chart
+		ax.text(i - .4, v + .3, str(v) + '%', color=set_color, fontweight='bold') #puts percentage of text above graph
 
 
-def make_graphs(array):
+def make_four_graphs(array):
 	ct = 0
-	colors = ("blue", "green", "red", "purple")
-	for i in len(array):
-		make_graph(array[i], colors[ct])
-		ct++
-		
-make_graph("India", "blue", 0)
-make_graph("America", "green", 1)
-make_graph("China", "red", 2)
-make_graph("Japan", "purple", 3)
+	colors = ["blue", "green", "red", "purple"]
+	print('foo')
+	for i in range(len(array)):
+		make_graph(array[i], colors[ct], i)
+		ct += 1
 
-plt.show()
+''' CALL FROM EXTERNAL PROGRAM LIKE THIS
+from display.py import graph4
+a = ["India", "America", "China", "Japan"]
+graph(a)
+'''
+def graph4(array):
+	plt.rcdefaults()
+	plt.suptitle('(CIA FACTBOOK 2016)')
+	make_four_graphs(array)
+	plt.show()
 
+def main():
+	graph4(["India", "America", "China", "Japan"])
+
+if __name__ == '__main__':
+	main()
