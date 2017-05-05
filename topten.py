112ff8f835035b1222bc9d7843bafcfbcc7e5e44
Patrick Carroll <Patrick Carroll>
2017-05-05 11:23:36 -0700

    Refactored topten.py functions besides the GUI

--- topten.py
+++ topten.py
@@ -12,33 +12,29 @@ FOUR = "55-64 years:"
 FIVE = "65 years and over:"
 regex = (r'\d{,2}\.?\d{,2}%')
 
-def first_ten_countries():
-	with open('parsed_age_data.json') as data:
-		d = json.load(data)
-		print(type(d))
-		for i in range(0, 9):
-			print(d[i][ONE])
-
+''' rtrn format: [1.0, 32.4, 2.0, 37.4, 3.0, 56.32...]'''
 def create_array(age_range):
-	rt = []
+	rtrn = []
 	with open('parsed_age_data.json') as data:
 		d = json.load(data)
 		for i in range(0, len(d)):
-			rt.append(float(i))
-			percentage = d[i][age_range]
-			rt.append(float(percentage[:len(percentage)-1]))
-	return rt
+			rtrn.append(float(i)) # 1.0, 2.0, 3.0, @ [0], [2], [4]. THIS SERVES AS AN INDEX KEY!!!
+			percentage = d[i][age_range] #gets 32.43%
+			rtrn.append(float(percentage[:len(percentage)-1])) # 32.43, 53.12, 5.46 @ [1], [3], [5]
+	return rtrn
 
-def sort_by_least_age_range(array, age_range):
+'''rtrn a sorted array format: [32.0, 61.22, 3.0, 56.32, 132.0, 54.28...]'''
+def sort_by_least_age_range(array):
 	a = array
-	for i in range(3, len(a), 2):
-		for j in range(1, i, 2):
+	for i in range(3, len(a), 2): # skips the first, i is the PERCENTAGE
+		for j in range(1, i, 2): # j iterates through the sorted PERCENTAGES
 			if a[i] < a[j]:
-				a.insert(j-1, a.pop(i))
-				a.insert(j-1, a.pop(i))
+				a.insert(j-1, a.pop(i)) # this moves the PERCENTAGE
+				a.insert(j-1, a.pop(i)) # this moves the INDEX KEY
 	return a
 
-def sort_by_greatest_age_range(array, age_range):
+'''could factor this out'''
+def sort_by_greatest_age_range(array):
 	a = array
 	for i in range(3, len(a), 2):
 		for j in range(1, i, 2):
@@ -47,26 +43,22 @@ def sort_by_greatest_age_range(array, age_range):
 				a.insert(j-1, a.pop(i))
 	return a
 
+'''prints in the terminal'''
 def print_top_x(array, age_range, num=0):
 	a = array
-	if num==0:
-		num = int(len(array)/2)
+	if num==0: # if no number is specified
+		num = int(len(array)/2) # prints everything
 	print('Age Range: {}'.format(age_range))
 	with open('parsed_age_data.json') as data:
 		d = json.load(data)
 		for i in range(0, num*2, 2):
-			index = (int)(a[i])
-			c = d[index]["Country"]
-			sys.stdout.write('{}.: {}\n%: {}'.format(int(i/2+1), c, a[i+1]))
+			index_key = (int)(a[i]) #
+			country = d[index_key]["Country"]
+			sys.stdout.write('{}. {}\n\t\t\t\t\t{}%'.format(int(i/2+1), country, a[i+1])) # 1: Azerbajan \n 32.60%
 			sys.stdout.write('\n')
 		sys.stdout.write('\n\n')
 		sys.stdout.flush()
 
-
-def menu():
-	root = tk.Tk()
-
-	root.mainloop()
 
 class Application(tk.Frame):
 
@@ -85,7 +77,7 @@ class Application(tk.Frame):
 			x = self.d["frame"+item] = tk.Frame(self, width=250, height=100)
 			self.d["label"+item] = tk.Label(x, text=item).pack(side=tk.LEFT)
 			self.d["most"+item] = tk.Button(x, text='most')
-			self.d["most"+item]["command"] = lambda: self.button_press("{}".format(item), int(self.entry.get()), True)
+			self.d["most"+item]["command"] = lambda: self.button_press(item, int(self.entry.get()), True)
 			self.d["most"+item].pack(side=tk.LEFT)
 			self.d["least"+item] = tk.Button(x, text='least')
 			self.d["least"+item]["command"] = lambda: self.button_press(item, int(self.entry.get()), False)
@@ -94,9 +86,9 @@ class Application(tk.Frame):
 
 	def button_press(self, action, num=10, greatest=True):
 		if greatest:
-			array = sort_by_greatest_age_range(create_array(action), action)
+			array = sort_by_greatest_age_range(create_array(action))
 		else:
-			array = sort_by_least_age_range(create_array(action), action)
+			array = sort_by_least_age_range(create_array(action))
 		print_top_x(array, action, num)
 
 root = tk.Tk()
